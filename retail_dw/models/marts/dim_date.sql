2dff333443e5dad7a1fa9694906f7dabf8de82bb
johncy-25 <johnsonrajendranraj@gmail.com>
2024-04-18 13:20:37 +0530

    Added column details in schema.yml file for marts folder

--- retail_dw/models/marts/dim_date.sql
+++ retail_dw/models/marts/dim_date.sql
@@ -1,7 +1,14 @@
 
-with date_dim as (
-    {{ dbt_date.get_date_dimension("2022-01-01", "2024-12-31") }}
-)
 select
-    *
-from date_dim
+  date_day,
+  day_of_week,
+  day_of_week_name,
+  day_of_week_name_short,
+  day_of_month,
+  day_of_year,
+  week_of_year,
+  month_of_year,
+  month_name,
+  month_name_short,
+  year_number
+from {{ ref("stg_date") }}
