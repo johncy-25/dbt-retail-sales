ac7a477e2cf035e9fed1071a9cc200358cb5c37f
johncy-25 <johnsonrajendranraj@gmail.com>
2024-04-18 12:09:49 +0530

    corrected fct_orders model

--- retail_dw/models/marts/fct_orders.sql
+++ retail_dw/models/marts/fct_orders.sql
@@ -4,7 +4,7 @@ select
   {{ dbt_utils.generate_surrogate_key(['product_asin','product_sku']) }} as product_key,
   {{ dbt_utils.generate_surrogate_key(['postal_code','city','state','country','ship_service_level']) }} as shipping_key,
   {{ dbt_utils.generate_surrogate_key(['fulfillment_type','fulfilled_by','sales_channel']) }} as fulfillment_key,
-  to_date(order_date,'MM-DD-YY'),
+  to_date(order_date,'MM-DD-YY') as order_date,
   order_status,
   courier_status,
   order_quantity,
